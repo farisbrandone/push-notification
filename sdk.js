729581726a2a1217bb2bd3516c701a86bc7444e4
kouetessa <farisbrandone@yahoo.com>
2024-09-30 08:18:18 +0100

    gg10

--- sdk.js
+++ sdk.js
@@ -76,12 +76,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/fireba
                     // You can send it to server, e.g. fetch('your.server/subscribe', { token });
                     // And store it for further usages (Server, LocalStorage, IndexedDB, ...)
                     // For example:
-                    alert(token)
+                    
                     var copyText = document.getElementById("myInput");
                     copyText.innerText=token
-                    var button =document.querySelector(".monbutton")
-                    button.innerHTML=token
-                  
                     window.localStorage.setItem("fcm_token", token);
                   })
                   .catch((err) => {
